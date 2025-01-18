21fe8c9d929e7add176f0a5920efee1f564b8baa
Nolan Corcoran <nd.corc@gmail.com>
2025-01-18 09:56:04 -0600

    structure

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -2,9 +2,7 @@ import { type SchemaTypeDefinition } from 'sanity';
 import post from './post';
 import category from './category';
 import project from './project';
-import caseStudy from './caseStudy';
-import architectureDiagram from './architectureDiagram';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-    types: [project, caseStudy, architectureDiagram, post, category]
+    types: [project, post, category]
 };
